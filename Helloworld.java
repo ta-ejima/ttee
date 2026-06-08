7bde55fdafcdcfbb3b8d33f6390b80d07ed8ac87
ta-ejima <small7pandada@gmail.com>
2026-06-08 15:46:03 +0900

    Create Helloworld.java
    GitHub 勉強

---
+++ Helloworld.java
@@ -0,0 +1,8 @@
+// クラスの定義
+public class HelloWorld {
+   // メインメソッド（プログラムのエントリーポイント）
+   public static void main(String[] args) {
+       // コンソールに文字列を出力
+       System.out.println("こんにちは、世界！");
+   }
+}
